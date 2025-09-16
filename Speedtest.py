7d2b513bffa91e8103e99940f53cef3f6ecf54d4
Meysam-Rajabipour <Meysam.rajabi@live.com>
2025-09-16 10:55:17 +0200

    update File

--- Speedtest.py
+++ Speedtest.py
@@ -1,6 +1,6 @@
 # To use this script, ensure you have installed the speedtest-cli package:
 # Run the following command in your terminal:
-pip install speedtest-cli
+# pip install speedtest-cli
 
 import speedtest
 
@@ -8,14 +8,17 @@ import speedtest
 st = speedtest.Speedtest()
 
 # Get best server based on ping
-st.get_best_server()
+best_server = st.get_best_server()
 
 # Perform download and upload speed tests
 download_speed = st.download() / 1_000_000  # Convert from bits to Mbps
 upload_speed = st.upload() / 1_000_000      # Convert from bits to Mbps
 ping = st.results.ping
 
-# Print results 01
-print(f"Download Speed: {download_speed:.2f} Mbps")
-print(f"Upload Speed:   {upload_speed:.2f} Mbps")
-print(f"Ping:           {ping:.2f} ms")
+# Print results
+print(f"Best Server:     {best_server['host']} ({best_server['name']}, {best_server['country']})")
+print(f"Server IP:       {best_server['ip']}")
+print(f"Download Speed:  {download_speed:.2f} Mbps")
+print(f"Upload Speed:    {upload_speed:.2f} Mbps")
+print(f"Ping:            {ping:.2f} ms")
+# Print the full results
